a77be874cc19218c80b528587361df388ae05257
Tim_LRN2016 <2941330150@qq.com>
2020-10-18 16:10:42 +0800

    hide accesskey

--- oss/ossreadfix.php
+++ oss/ossreadfix.php
@@ -11,8 +11,8 @@ use OSS\Core\OssException;
 function readOSS($url,$bucket){
 	
 // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
-$accessKeyId = "LTAI4FiPTWLJEGBF17YrdaAu";
-$accessKeySecret = "gYFIeOXW0tV1JIWTH8gIu78BWBbHG0";
+$accessKeyId = "$accessKeyId";
+$accessKeySecret = "$accessKeyId";
 // Endpoint以杭州为例，其它Region请按实际情况填写。
 $endpoint = "http://oss-cn-beijing.aliyuncs.com";
 
